68299085e02d1f7030c2cdde0d6ff25d85f8b41e
Afraa Shaikh <shaikhafraabi@gmail.com>
2026-06-23 13:09:31 +0530

    fix: reduce about-asset size in hero

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -60,7 +60,7 @@ export default function AboutPage() {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.3, ease: [0.22, 1, 0.36, 1] }}
-            className="hidden md:block w-[500px] lg:w-[640px] xl:w-[780px] object-contain select-none pointer-events-none"
+            className="hidden md:block w-[260px] lg:w-[320px] xl:w-[380px] object-contain select-none pointer-events-none"
           />
 
         </div>
